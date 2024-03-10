6b42282d6ea4ceaa5de9253cf87a857bffd067c5
MUHAMMAD FADLI <bangpaik1@gmail.com>
2024-03-10 11:20:31 +0700

    Flutter awal

--- README.md
+++
@@ -1,2 +0,0 @@
-# PROJECT-EDUKASI
- Mini Case Tim 5
---
+++ project_edukasi/android/app/src/main/kotlin/com/project_edukasi/project_edukasi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.project_edukasi.project_edukasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
